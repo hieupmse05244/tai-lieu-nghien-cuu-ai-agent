e7032256c00b4b221b247828c6ed95115d80abc2
hieupm <hieupm.98.tn@gmail.com>
2026-03-25 08:40:50 +0700

    Remove all env files from tracking

--- 07-Experiments/Daily-Reminder-System/backend/dist/dtos/base-dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseDTO = void 0;
-class BaseDTO {
-    constructor(partial) {
-        Object.assign(this, partial);
-    }
-}
-exports.BaseDTO = BaseDTO;
--- 07-Experiments/Daily-Reminder-System/backend/dist/modules/notification/mappers/telegraf-command.mapper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mapToNotificationMessage = void 0;
-const mapToNotificationMessage = (task) => {
-    const assignee = task.assignees[task.rotationIndex];
-    return `Task '${task.title}' is currently assigned to ${assignee}. Follow up as per the schedule.`;
-};
-exports.mapToNotificationMessage = mapToNotificationMessage;
